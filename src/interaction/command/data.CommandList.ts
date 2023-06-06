4d90e46b4768e6b46ac757cf62a16be2922c8759
michael <ydna.mjs@gmail.com>
2023-06-06 18:45:55 -0400

    Disabled discussion menu command since it isnt currently being used for anything anyway

--- src/interaction/command/data.CommandList.ts
+++ src/interaction/command/data.CommandList.ts
@@ -2,11 +2,10 @@
 import { Command } from "./interface.Command";
 
 //import command from its respective file
-import { DiscussionMenu } from "./menu/Command.DiscussionMenu";
-//import { RegisterDiscussionCourse } from "./menu/Command_RegisterDiscussionCourse";
+//import { DiscussionMenu } from "./menu/Command.DiscussionMenu"; // not currently in use but will be in the final version
 import { testMenu } from "./menu/Command.TestMenu";
 
 //list of all the available commands
-export const commandList: Command[] = [DiscussionMenu, testMenu];
+export const commandList: Command[] = [testMenu];
 
 //some code taken from https://sabe.io/tutorials/how-to-build-discord-bot-typescript
