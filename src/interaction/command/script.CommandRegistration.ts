eff68f885523e439c0f3e9a6e163a8033ac9fded
michael <ydna.mjs@gmail.com>
2023-06-06 18:43:40 -0400

    Fixed command registration script

--- src/interaction/command/script.CommandRegistration.ts
+++ src/interaction/command/script.CommandRegistration.ts
@@ -1,98 +1,77 @@
-const { REST, Routes } = require('discord.js');
-import { DISCORD_TOKEN, CLIENT_ID, NAZIM_TEST_SERVER_ID, MY_SERVER_ID } from "../../secret";
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+import { DISCORD_TOKEN, NAZIM_TEST_SERVER_ID, MY_SERVER_ID } from "../../secret";
 import { commandList } from "./data.CommandList";
-
-
-// CONSTANT
-const rest = new REST().setToken(DISCORD_TOKEN);
 
 // MAIN CALL
 main(process.argv[2], process.argv[3]);
 
-// HELPER FUNCTIONS
-function main(action: string, server: string) {
-    if(action === "register") {
-        switch(server) { 
-            case "global": { 
-                registerAllCommandsToGlobal();
-                break; 
-            } 
-            case "nazim": { 
-                registerAllCommandsToNazim();
-               break; 
-            } 
-            case "ydna": {
-                registerAllCommandsToYdna();
-                break;
+async function main(action: string, server: string) {
+
+    // TODO: some of the stuff here can probably be removed. Not super urgent but if you have free time its something to do
+    const client = new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.DirectMessages,
+        ],
+        partials: [
+            //not really sure what this is but it's needed for the bot to recognize when it recieves a DM for some reason
+            Partials.Channel
+        ]
+    });
+    
+    client.on("ready", async () => {
+        if (client.user && client.application) {
+        
+            if(action === "register") {
+                switch(server) { 
+                    case "global": { 
+                        await client.application.commands.set(commandList);
+                        console.log('Successfully registered all application commands to global');
+                        process.exit();
+                    } 
+                    case "nazim": { 
+                        await client.application.commands.set(commandList, NAZIM_TEST_SERVER_ID);
+                        console.log('Successfully registered all application commands to Nazim\'s server');
+                        process.exit();
+                    } 
+                    case "ydna": {
+                        await client.application.commands.set(commandList, MY_SERVER_ID);
+                        console.log('Successfully registered all application commands to ydna\'s server');
+                        process.exit();
+                    }
+                    default: { 
+                        console.warn("WARNING: Server parameter of command registration was invalid. Nothing was executed");
+                        process.exit();
+                    } 
+                } 
             }
-            default: { 
-                console.warn("WARNING: Server parameter of command registration was invalid. Nothing was executed");
-                break; 
-            } 
-         } 
-    }
-    else if (action === "unregister") {
-        switch(server) { 
-            case "global": { 
-                unregisterAllCommandsGlobal(); 
-                break; 
-            } 
-            case "nazim": { 
-                unregisterAllCommandsNazim();
-                break; 
-            } 
-            case "ydna": {
-                unregisterAllCommandsYdna();
-                break;
+            else if (action === "unregister") {
+                switch(server) { 
+                    case "global": { 
+                        await client.application.commands.set([]);
+                        console.log('Successfully deleted all application commands from global');
+                        process.exit(); 
+                    } 
+                    case "nazim": { 
+                        await client.application.commands.set([], NAZIM_TEST_SERVER_ID);
+                        console.log('Successfully deleted all application commands from Nazim\'s server');
+                        process.exit(); 
+                    } 
+                    case "ydna": {
+                        await client.application.commands.set([], MY_SERVER_ID);
+                        console.log('Successfully deleted all application commands from ydna\'s server');
+                        process.exit(); 
+                    }
+                    default: { 
+                        console.warn("WARNING: Server parameter of command registration was invalid. Nothing was executed");
+                        process.exit();
+                    } 
+                } 
             }
-            default: { 
-                console.warn("WARNING: Server parameter of command registration was invalid. Nothing was executed");
-                break; 
-            } 
-         } 
-    }
-}
+        }
+    });
 
-// Register commands to ydna's server
-function registerAllCommandsToGlobal() {
-    rest.put(Routes.applicationCommands(CLIENT_ID), { body: commandList })
-    .then(() => console.log('Successfully registered all application commands to global'))
-    .catch(console.error);
+    client.login(DISCORD_TOKEN);   
 }
-
-// Register commands to Nazim's server
-function registerAllCommandsToNazim() {
-    rest.put(Routes.applicationCommands(CLIENT_ID, NAZIM_TEST_SERVER_ID), { body: commandList })
-	.then(() => console.log('Successfully registered all application commands to Nazim\'s Server'))
-	.catch(console.error);
-}
-
-// Register commands to ydna's server
-function registerAllCommandsToYdna() {
-    rest.put(Routes.applicationCommands(CLIENT_ID, MY_SERVER_ID), { body: commandList })
-    .then(() => console.log('Successfully registered all application commands to ydna\'s Server'))
-    .catch(console.error);
-}
-
-// Unregister all global commands
-function unregisterAllCommandsGlobal() {
-    rest.put(Routes.applicationCommands(CLIENT_ID), { body: [] })
-	.then(() => console.log('Successfully deleted all application commands from global'))
-	.catch(console.error);
-}
-
-// Unregister all Nazim's server commands
-function unregisterAllCommandsNazim() {
-    rest.put(Routes.applicationCommands(CLIENT_ID, NAZIM_TEST_SERVER_ID), { body: [] })
-	.then(() => console.log('Successfully deleted all application commands from Nazim\'s server'))
-	.catch(console.error);
-}
-
-// Unregister all ydna's server commands
-function unregisterAllCommandsYdna() {
-    rest.put(Routes.applicationCommands(CLIENT_ID, MY_SERVER_ID), { body: [] })
-	.then(() => console.log('Successfully deleted all application commands from ydna\'s server'))
-	.catch(console.error);
-}
-
-// Some code taken from https://discordjs.guide/slash-commands/deleting-commands.html#deleting-specific-commands
