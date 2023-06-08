9626f140a508bbb23a31c4f26bca6ccec460b8a4
michael <ydna.mjs@gmail.com>
2023-06-08 19:31:18 -0400

    Added documentation for util.makeActionRow and Command.DiscussionMenu

--- src/interaction/command/menu/Command.DiscussionMenu.ts
+++ src/interaction/command/menu/Command.DiscussionMenu.ts
@@ -1,29 +1,21 @@
 import { CommandInteraction, Client, Interaction} from "discord.js";
 import { Command } from "../interface.Command";
 
-// constants
+//** @constant the message sent to the user indicating that a menu was sent to them */
 const MENU_SENT_MESSAGE = "Discussion menu was sent to your direct messages";
 
+/** @constant the command for getting a discussion menu sent to yourself */
 export const DiscussionMenu: Command = {
     name: "discussion-menu",
     description: "opens an embed menu for managing the discussion features",
     run: async (client: Client, interaction: CommandInteraction) => {
         
-        // CURRENTLY DOES NOTHING BECAUSE REFACTOR REMOVED THE IMPLEMENTATION
-
-        // Direct Message the user the discussion main menu
-        //const sentMenu = await interaction.user.send(mainMenu);
-        //const sentMenu = await interaction.user.send(mainMenu.getMessageComponent());
-
+        // TODO: Implement sending a discussion main menu once that menu is implemented
         
         // Let them know that they have been DM'd the discussion menu
         await interaction.followUp({
             ephemeral: true,
             content: MENU_SENT_MESSAGE
         });
-
-        // Handles the collection of button events for the menu
-        //collectButtonInteraction(client, interaction, sentMenu);
-        //mainMenu.collectButtonInteraction(client, interaction, sentMenu);
     }
 }
