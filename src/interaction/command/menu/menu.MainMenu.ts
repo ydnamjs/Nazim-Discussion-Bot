888d90952e170d7c8a56a2a260af4caf2e1257f1
michael <ydna.mjs@gmail.com>
2023-06-09 09:08:31 -0400

    Added documentation for the important parts of the main menu

--- src/interaction/command/menu/menu.MainMenu.ts
+++ src/interaction/command/menu/menu.MainMenu.ts
@@ -16,6 +16,7 @@ const MAIN_MENU_STAFF_BUTTON_LABEL = "staff view";
 const MAIN_MENU_STAFF_BUTTON_STYLE = ButtonStyle.Primary;
 const MAIN_MENU_STAFF_BUTTON_DISABLED = false;
 
+/** @constant list of button data used to construct the visual parts of the buttons for the main menu */
 const MAIN_MENU_BUTTON_DATA: Partial<ButtonComponentData>[] = [
     {
         customId: MAIN_MENU_STUDENT_BUTTON_ID,
@@ -31,8 +32,10 @@ const MAIN_MENU_BUTTON_DATA: Partial<ButtonComponentData>[] = [
     }
 ];
 
+/** @constant the components of the main menu (see MAIN_MENU_BUTTON_DATA for how editing how they look and MAIN_MENU_BUTTON_BEHAVIORS for how they act) */
 const MAIN_MENU_COMPONENTS = [makeActionRowButton(MAIN_MENU_BUTTON_DATA)];
 
+/**@constant behavior data of main menu buttons (see component behavior interface in class.BaseMenu.ts) */
 const MAIN_MENU_BUTTON_BEHAVIORS: ComponentBehavior[] = [
     {
         filter: (customId: string) => {
@@ -62,6 +65,7 @@ const MAIN_MENU_BUTTON_BEHAVIORS: ComponentBehavior[] = [
     },
 ]
 
+/** @constant the main menu object that is sent for the discussion menu command - used to acess all other menus */
 export const mainMenu = new BaseMenu({
     title: MAIN_MENU_TITLE,
     description: MAIN_MENU_DESCRIPTION,
