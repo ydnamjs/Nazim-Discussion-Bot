b4c7390b95f56da8468f70ab0279a55fff91fb84
michael <ydna.mjs@gmail.com>
2023-06-09 08:48:32 -0400

    Added buttons to the main menu currently disabled and have no behavior

--- src/interaction/command/menu/menu.MainMenu.ts
+++ src/interaction/command/menu/menu.MainMenu.ts
@@ -1,11 +1,37 @@
-import { BaseMenu } from "./class.BaseMenu";
+import { ButtonComponentData, ButtonStyle } from "discord.js";
+import { BaseMenu, buttonData } from "./class.BaseMenu";
+import { makeActionRowButton } from "./util.makeActionRow";
 
 const MAIN_MENU_TITLE = "Discussion Menu";
 const MAIN_MENU_DESCRIPTION = "Welcom to the discussion menu! Click the button below that corresponds to your role to open a menu for that role";
 const MAIN_MENU_FIELDS: {name: string,  value:string}[] = [];
 
+const MAIN_MENU_BUTTON_DATA: Partial<ButtonComponentData>[] = [
+    {
+        customId: "discussion_student_menu_button",
+        label: "student view",
+        style: ButtonStyle.Primary,
+        disabled: true
+    },
+    {
+        customId: "discussion_staff_menu_button",
+        label: "staff view",
+        style: ButtonStyle.Primary,
+        disabled: true
+    },
+    {
+        customId: "discussion_admin_menu_button",
+        label: "admin view",
+        style: ButtonStyle.Primary,
+        disabled: true
+    }
+]
+
+const MAIN_MENU_COMPONENTS = [makeActionRowButton(MAIN_MENU_BUTTON_DATA)];
+
 export const mainMenu = new BaseMenu({
     title: MAIN_MENU_TITLE,
     description: MAIN_MENU_DESCRIPTION,
-    fields: MAIN_MENU_FIELDS
+    fields: MAIN_MENU_FIELDS,
+    components: MAIN_MENU_COMPONENTS
 });
