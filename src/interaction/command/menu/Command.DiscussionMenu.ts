f947b2364a57d4e384f53c1b462092f7236a87a1
michael <ydna.mjs@gmail.com>
2023-06-09 08:40:25 -0400

    Connected the discussion menu command and main menu so that the command sends the user the menu to their direct messages

--- src/interaction/command/menu/Command.DiscussionMenu.ts
+++ src/interaction/command/menu/Command.DiscussionMenu.ts
@@ -1,8 +1,9 @@
 import { CommandInteraction, Client, Interaction} from "discord.js";
 import { Command } from "../interface.Command";
+import { mainMenu } from "./menu.MainMenu";
 
 //** @constant the message sent to the user indicating that a menu was sent to them */
-const MENU_SENT_MESSAGE = "Discussion menu was sent to your direct messages";
+const MENU_SENT_MESSAGE = "Discussion menu was sent to your direct messages: ";
 
 /** @constant the command for getting a discussion menu sent to yourself */
 export const DiscussionMenu: Command = {
@@ -12,10 +13,12 @@ export const DiscussionMenu: Command = {
         
         // TODO: Implement sending a discussion main menu once that menu is implemented
         
+        const messageLink = (await mainMenu.send(client, interaction)).url;
+
         // Let them know that they have been DM'd the discussion menu
         await interaction.followUp({
             ephemeral: true,
-            content: MENU_SENT_MESSAGE
+            content: MENU_SENT_MESSAGE + messageLink
         });
     }
 }
