292b47a0b241131bf4ed3d18e005b6f3aeef24f9
michael <ydna.mjs@gmail.com>
2023-06-10 12:20:56 -0400

    Added sage's add course command and updated it to work with v14. side note it works with the staff view!

--- src/interaction/command/data.CommandList.ts
+++ src/interaction/command/data.CommandList.ts
@@ -1,13 +1,14 @@
 // command interface
 import { Command } from "./interface.Command";
-import { DiscussionMenu } from "./menu/Command.DiscussionMenu";
 
 // Imported commands
+import { addCourse } from "./Command.addcourse";
+import { DiscussionMenu } from "./menu/Command.DiscussionMenu";
 import { testMenu } from "./menu/Command.TestMenu";
 
 /**
  * @constant List of all the available commands
 */
-export const commandList: Command[] = [DiscussionMenu, testMenu];
+export const commandList: Command[] = [ addCourse, DiscussionMenu, testMenu];
 
 //some code taken from https://sabe.io/tutorials/how-to-build-discord-bot-typescript
