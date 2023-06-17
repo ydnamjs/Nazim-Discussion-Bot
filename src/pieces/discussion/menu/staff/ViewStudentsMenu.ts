f9cd506af246914f0c056d2f9fb14bab4f819b86
michael <ydna.mjs@gmail.com>
2023-06-17 17:50:19 -0400

    Made it so that update to view students now gets every student in the course by checking if every member in the guild has the student role of the course. As such all students of the course are listed instead of filler text

--- src/pieces/discussion/menu/staff/ViewStudentsMenu.ts
+++ src/pieces/discussion/menu/staff/ViewStudentsMenu.ts
@@ -1,18 +1,44 @@
-import { InteractionUpdateOptions, Message, MessageComponentInteraction } from "discord.js";
+import { Guild, InteractionUpdateOptions, Message, MessageComponentInteraction } from "discord.js";
 import { NavigatedMenu, NavigatedMenuData } from "../NavigatedMenu";
+import { Course, courseModel } from "../../../../generalModels/Course";
+import { ROLES_GUILD } from "../../../../secret";
 
 /**
  * @function updates a menu so that it is now a staff menu
+ * @param {string} courseTitle - the title of the course whose students are to be viewed
  * @param {Message} message - the message to have the menu be replaced on
  * @param {MessageComponentInteraction} componentInteraction - the interaction that triggered this menu replacement
  */
 export async function updateToViewStudentsMenu(courseTitle: string, message: Message, componentInteraction: MessageComponentInteraction) {
 
-    const studentData: DiscussionStudentData[] = [
-        {
-            username: "fred"
-        },
-    ]
+    // get the ids of all students in the course
+    let course: Course | null = null;
+    try {
+        course = await courseModel.findOne({name: courseTitle});
+    }
+    catch(error: any) {
+        console.error(error);
+    }
+
+    // if the course was not found there was a problem getting it from the database
+    if(!course) {
+        componentInteraction.reply("Database error. Please message admin");
+        return;
+    }
+
+    // get userIds of every student in the course
+    const guild = componentInteraction.client.guilds.cache.get(ROLES_GUILD) as Guild;
+    const studentIds =  guild.members.cache.filter((member) => {
+        return [...member.roles.cache.keys()].includes(course?.roles.student as string)
+    })
+
+    // generate a list of student data from student Ids
+    const studentData: DiscussionStudentData[] = []
+    studentIds.forEach((student) => {
+        studentData.push({
+            username: student.user.username
+        })
+    })
 
     // replace the old menu with the view students menu
     const viewStudentsMenu = new ViewStudentsMenu(courseTitle, studentData);
