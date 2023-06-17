a56cea6aaa64b4df7c0fe9412e74522cf144ba2d
michael <ydna.mjs@gmail.com>
2023-06-17 16:27:02 -0400

    Finished default specifications for discussion scoring

--- src/pieces/courseManagement/DiscussionRulesDefaults.ts
+++ src/pieces/courseManagement/DiscussionRulesDefaults.ts
@@ -1,5 +1,8 @@
-const DEFAULT_POST_RULES = {
+import { CommentSpecs, DiscussionSpecs, PostSpecs } from "../../generalModels/DiscussionScoring";
+
+const DEFAULT_POST_SPECS: PostSpecs = {
     points: 250,
+    commentPoints: 60,
     minLength: 50,
     minParagraphs: 0,
     minLinks: 0,
@@ -13,14 +16,47 @@ const DEFAULT_POST_RULES = {
         // default silver award
         {
             reaction: ":second_place:",
-            points: 100,
+            points: 50,
             trackStudents: false
         },
         // default bronze award
         {
             reaction: ":third_place:",
+            points: 20,
+            trackStudents: false
+        },
+    ]
+}
+
+const DEFAULT_COMMENT_SPECS: CommentSpecs = {
+    points: 250,
+    minLength: 25,
+    minParagraphs: 0,
+    minLinks: 0,
+    awards: [
+        // default gold award
+        {
+            reaction: ":first_place:",
             points: 100,
             trackStudents: false
-        },        
-    ]
+        },
+        // default silver award
+        {
+            reaction: ":second_place:",
+            points: 50,
+            trackStudents: false
+        },
+        // default bronze award
+        {
+            reaction: ":third_place:",
+            points: 20,
+            trackStudents: false
+        },
+    ]   
+}
+
+export const DEFAULT_DISCUSSION_SPECS: DiscussionSpecs = {
+    postSpecs: DEFAULT_POST_SPECS,
+    commentSpecs: DEFAULT_COMMENT_SPECS,
+    scorePeriods: []
 }
