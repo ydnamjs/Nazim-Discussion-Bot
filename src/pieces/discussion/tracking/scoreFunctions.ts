3302c147b198b95b88d92a190ba72bc0e28d64e3
michael <ydna.mjs@gmail.com>
2023-06-18 16:37:59 -0400

    Added an interface for storing score requirement info, a function for counting paragraphs, and finished the function for scoring a comment

--- src/pieces/discussion/tracking/scoreFunctions.ts
+++ src/pieces/discussion/tracking/scoreFunctions.ts
@@ -1,14 +1,52 @@
 import { Message } from "discord.js";
-import { CommentSpecs } from "src/generalModels/DiscussionScoring";
+import { CommentSpecs } from "../../../generalModels/DiscussionScoring";
 
-function scoreComment(comment: Message, commentSpecs: CommentSpecs): number {
+/**
+ * @interface collection of what requirements were met for a comment or post
+ * @property {boolean} passedLength - whether the post or comment met the length requirement specified in the course it belongs to
+ * @property {boolean} passedParagraph - whether the post or comment met the paragraph requirement specified in the course it belongs to
+ * @property {boolean} passedLinks = whether the post or comment met the link requirement specified in the course it belongs to
+ */
+interface ScoreChecks {
+    passedLength: boolean,
+    passedParagraph: boolean,
+    passedLinks: boolean
+}
+
+function scoreComment(comment: Message, commentSpecs: CommentSpecs): {score: number, scoreChecks: ScoreChecks} {
     
     // remove multiple new lines in a row and newlines and spaces at the end
     const contentTrimmed = (comment.content.replace(/[\r\n]+/g, '\n')).trim();
+    // remove all empty spaces
+    const contentNoEmpty = contentTrimmed.replace(/[\s]+/g, '');
 
-    if(contentTrimmed.length >= commentSpecs.minLength && contentTrimmed.)
+    let score = 0;
+    const scoreChecks = {
+        passedLength: contentNoEmpty.length >= commentSpecs.minLength,
+        passedParagraph: countParagraphs(contentTrimmed) >= commentSpecs.minParagraphs,
+        passedLinks: countLinks(contentTrimmed) >= commentSpecs.minLinks
+    }
+
+    if(scoreChecks.passedLength && scoreChecks.passedParagraph && scoreChecks.passedLinks) {
+        score = commentSpecs.points;
+    }
     
-    return score;
+    return {score: score, scoreChecks: scoreChecks};
+}
+
+/**
+ * @function counts the number of paragraphs in a string
+ * @param {string} content - the string to count the paragraphs of
+ * @returns {number} - the number of paragraphs in the string
+ */
+function countParagraphs(content: string):number {
+    const countArr =  content.match(/\n+/g || [])
+    
+    if(!countArr) {
+        return 0;
+    }
+    
+    return countArr.length;
 }
 
 /**
@@ -17,7 +55,7 @@ function scoreComment(comment: Message, commentSpecs: CommentSpecs): number {
  * @returns {number} - the number of links in the string
  */
 function countLinks(content: string):number {
-    const countArr =  content.match(/http/g || [])
+    const countArr =  content.match(/http+/g || [])
     
     if(!countArr) {
         return 0;
