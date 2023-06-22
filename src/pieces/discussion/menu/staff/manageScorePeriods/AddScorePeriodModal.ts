b53200c80c9b902742ae53389376d61bb8fa78e4
michael <ydna.mjs@gmail.com>
2023-06-22 03:23:19 -0400

    Improved comments in add score period modal

--- src/pieces/discussion/menu/staff/manageScorePeriods/AddScorePeriodModal.ts
+++ src/pieces/discussion/menu/staff/manageScorePeriods/AddScorePeriodModal.ts
@@ -64,33 +64,51 @@ const endDateActionRow = new ActionRowBuilder<TextInputBuilder>({components: [en
 const goalPointsActionRow = new ActionRowBuilder<TextInputBuilder>({components: [goalPointsInput]});
 const maxPointsActionRow = new ActionRowBuilder<TextInputBuilder>({components: [maxPointsInput]});
 
-interface scorePeriodInputData {
+/**
+ * @interface defines the scorePeriod data that has been input. Used for after it has been converted from strings
+ * @property {Date | undefined} startDate - If valid, the start date and time of the score period Undefined. if invalid input
+ * @property {Date | undefined} endDate - If valid, the end date and time of the score period. Undefined if invalid input
+ * @property {number} goalPoints - The target score that students are expected to earn in the score period. NaN if invalid input
+ * @property {number} maxPoints - The maximum number of points that students can in earn in the score period. NaN if invalid input
+ */
+interface ScorePeriodInputData {
     startDate: Date | undefined, 
     endDate: Date | undefined,  
     goalPoints: number, 
     maxPoints: number
 }
 
-function validateInput(submittedModal: ModalSubmitInteraction): scorePeriodInputData {
-    const startDateString = submittedModal.fields.getTextInputValue(START_DATE_INPUT_ID);
+/**
+ * @function validates the input of an add score period modal
+ * @param addScorePeriodModal the submitted modal whose input is to be validated
+ * @returns {ScorePeriodInputData} scorePeriodInputData - the score period input data after validation (see ScorePeriodInputData interface)
+ */
+function validateInput(addScorePeriodModal: ModalSubmitInteraction): ScorePeriodInputData {
+    const startDateString = addScorePeriodModal.fields.getTextInputValue(START_DATE_INPUT_ID);
     const startDateTime = DateTime.fromFormat(startDateString, "yyyy-MM-dd hh:mm a")
     const startDate = startDateTime.isValid ? startDateTime.toJSDate() : undefined;
 
-    const endDateString = submittedModal.fields.getTextInputValue(END_DATE_INPUT_ID);
+    const endDateString = addScorePeriodModal.fields.getTextInputValue(END_DATE_INPUT_ID);
     const endDateTime = DateTime.fromFormat(endDateString, "yyyy-MM-dd hh:mm a")
     const endDate = startDateTime.isValid ? endDateTime.toJSDate() : undefined;
 
-    const goalPoints = parseInt(submittedModal.fields.getTextInputValue(GOAL_POINTS_INPUT_ID));
-    const maxPoints = parseInt(submittedModal.fields.getTextInputValue(MAX_POINTS_INPUT_ID));
+    const goalPoints = parseInt(addScorePeriodModal.fields.getTextInputValue(GOAL_POINTS_INPUT_ID));
+    const maxPoints = parseInt(addScorePeriodModal.fields.getTextInputValue(MAX_POINTS_INPUT_ID));
 
     return {startDate: startDate, endDate: endDate, goalPoints: goalPoints, maxPoints: maxPoints};
 }
 
+/**
+ * @function opens an add score period modal for the given course as a result of the given interaction
+ * @param courseTitle - the title of the corse that the score period is to be added to 
+ * @param interaction - the interaction that triggered the opening of this modal
+ */
 export async function openAddScorePeriodModal(courseTitle: string, interaction: ButtonInteraction) {
     
+    // refresh the manage score periods menu so that after the modal is close/submitted it collects input again
     await updateToManageScorePeriodsMenu(courseTitle, interaction, false);
     
-    // MODAL
+    // give the user a modal to input data to
     const addScorePeriodModal = new ModalBuilder({
         customId: "add-score-period-modal",
         title: ADD_SCORE_MODAL_TITLE_PREFIX + courseTitle,
@@ -103,16 +121,19 @@ export async function openAddScorePeriodModal(courseTitle: string, interaction: 
     })
     interaction.showModal(addScorePeriodModal);
 
-    // COLLECT MODAL
+    // collect data from the modal
     let submittedModal: ModalSubmitInteraction | undefined = undefined;
     try {
         submittedModal = await interaction.awaitModalSubmit({time: ADD_SCORE_PERIOD_MODAL_EXPIRATION_TIME})
     }
     catch {}
 
+    // if the modal is submitted, process the data given
     if (submittedModal !== undefined) {
+        
         const modalData = validateInput(submittedModal);
 
+        // if the data is valid
         if(modalData.startDate && modalData.endDate && !Number.isNaN(modalData.goalPoints) && !Number.isNaN(modalData.maxPoints)) {
             
             const newStart = modalData.startDate;
@@ -125,8 +146,6 @@ export async function openAddScorePeriodModal(courseTitle: string, interaction: 
             catch(error: any) {
                 console.error(error);
             }
-            
-            modalData.startDate
 
             if(course) {
                 const scorePeriods = course.discussionSpecs?.scorePeriods;
@@ -158,19 +177,17 @@ export async function openAddScorePeriodModal(courseTitle: string, interaction: 
                         {discussionSpecs: disc}
                     )
                     submittedModal.reply("New Score Period Added!");
+
+                    //TODO: After a score period is added, score all of the posts and comments that would fall into it (only necessary for score periods that started in the past)
+
                 }
 
             }
         }
+        // if the data is not valid, inform the user
         else {
             submittedModal.reply("Invalid Input Format. New Score Period Was Not Added");
         }
 
     }
-
-    // PROCESS INPUT
-    // TODO: implement me
-
-
-
 }
