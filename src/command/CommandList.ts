5083007960e9c63ce5a24b92aedfcc46f21ea2e9
michael <ydna.mjs@gmail.com>
2023-06-29 19:31:29 -0400

    deleted test score command because it was no longer useful

--- src/command/CommandList.ts
+++ src/command/CommandList.ts
@@ -2,8 +2,7 @@ import { Command } from "./Command";
 import { addCourse } from "../pieces/courseManagement/Addcourse";
 import { DiscussionMenu } from "../pieces/discussion/menu/DiscussionMenu";
 import { removeCourse } from "../pieces/courseManagement/RemoveCourse";
-import { testScore } from "../pieces/discussion/tracking/testScoreCommand";
 
-export const commandList: Command[] = [ addCourse, removeCourse, DiscussionMenu, testScore];
+export const commandList: Command[] = [ addCourse, removeCourse, DiscussionMenu];
 
 //some code taken from https://sabe.io/tutorials/how-to-build-discord-bot-typescript
