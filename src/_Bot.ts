a537a7fab6899d62701ce2a1d40c6c57d2411760
michael <ydna.mjs@gmail.com>
2023-06-29 19:21:26 -0400

    Cleaned up bot file

--- src/_Bot.ts
+++ src/_Bot.ts
@@ -1,22 +1,11 @@
 import { Client, GatewayIntentBits, Partials } from "discord.js";
-import { DISCORD_TOKEN, MONGODB_SRV } from "./secret";
-
-//import event listeners
+import mongoose, { ConnectOptions } from "mongoose";
 import commandListener from "./listeners/InteractionListener";
 import onlineLogger from "./listeners/ReadyListener";
+import { DISCORD_TOKEN, MONGODB_SRV } from "./secret";
 
-// TODO: converting this to an import disalows the useNewUrlParser option for some reason. When done with refactoring figure out if converting breaks it or not
-//import database connection thing
-const mongoose = require("mongoose");
-
-
-// MAIN CALL
 main();
 
-/**
- * @function The main function of the entire project.
- * This is the actual running bot.
- */
 async function main() {
 
     console.log("Bot is starting...");
@@ -35,34 +24,23 @@ async function main() {
         ]
     });
 
-    //connect to database
     console.log("Attempting to connect to the database...");
     await mongoose.connect(
         MONGODB_SRV, {
             useNewUrlParser: true,
             useUnifiedTopology: true
-        }
+        } as ConnectOptions
     ).then(()=>{
         console.log("Successfully connected to the database!")
     }).catch((err: Error) => {
         console.log(err);
     });
 
-    // add the listeners to the client so that it can interact with the world
     addListeners(client);
 
-    //log in to discord
     client.login(DISCORD_TOKEN);
 }
-    
-//function that adds all of the event listeners from the listeners folder
-/**
- * @function Adds event listeners to the given client
- * 
- * @param {Client} client - The client to add event listeners to
- * 
- * @note listeners are defined and imported from subfolder:  "interaction"
- */
+
 function addListeners(client: Client) {
     console.log("adding listeners...");
     onlineLogger(client);
