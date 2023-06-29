d047a10b1b15e1b8dd843214cb4dfceadea81787
michael <ydna.mjs@gmail.com>
2023-06-29 18:54:57 -0400

    Cleaned up listeners comments

--- src/listeners/InteractionListener.ts
+++ src/listeners/InteractionListener.ts
@@ -1,48 +1,31 @@
 import { Client, CommandInteraction, Interaction } from "discord.js";
 import { commandList } from "../command/CommandList";
 
-// constants
-/**
- * @constant - The message to reply to the user with when they send a command that does not exist
- */
 const COMMAND_NOT_FOUND_MESSAGE = "Error: Command Not Found. Please contact an admin"
 
 /** 
  * @function creates a listener for interactions on the given client
- * 
  * @param {Client} client - the client whose interactions should be listened for
 */
 export default (client: Client): void => {
+    
     client.on("interactionCreate", async (interaction: Interaction) => {
-        
-        // handle commands
+
         if (interaction.isCommand() || interaction.isContextMenuCommand()) {
             await handleSlashCommand(client, interaction);
         }
-
     });
 };
 
-/**
- * 
- * @function handles finding and running a command for the client
- * 
- * @param {Client} client - the client to run the command for
- * 
- * @param {CommandInteraction} interaction - the interaction with the command to run
- */
 async function handleSlashCommand(client: Client, interaction: CommandInteraction): Promise<void> {
     
-    // find the command in the list
     const slashCommand = commandList.find(c => c.name === interaction.commandName);
 
-    // if it doesnt exist there is an error and we return
     if (!slashCommand) {
         interaction.reply({ content: COMMAND_NOT_FOUND_MESSAGE });
         return;
     }
 
-    // if the command does exist run the command
     await interaction.deferReply();
     slashCommand.run(client, interaction);
 };
--- src/listeners/ReadyListener.ts
+++ src/listeners/ReadyListener.ts
@@ -4,18 +4,21 @@ import { GUILDS } from "../secret";
 
 /**
  * @function Creates a ready listener for the client given
- * 
  * @param {Client} client - the client to create the ready listener for
  */
 export default (client: Client): void => {
-    client.on("ready", async () => {
+    client.once("ready", async () => {
+        
         if (!client.user || !client.application) {
             return;
         }
-        // TODO: idk how bad this is but it seems essential if discussions are to be able to get info about people who havent interacted with the bot since its last restart
+        
         console.log("fetching members");
+        
+        // we fetch all of the members here so that we dont have issue pulling from the cache of members anywhere else
         const guild = client.guilds.cache.get(GUILDS.MAIN) as Guild;
         await guild.members.fetch();
+        
         console.log("all members fetched");
         console.log(`${client.user.username} is online!`);
     });
