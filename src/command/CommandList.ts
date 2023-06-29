715c4b1df85d68119b3f2062477f6561fbf42456
michael <ydna.mjs@gmail.com>
2023-06-29 19:17:13 -0400

    Cleaned up command files

--- src/command/CommandList.ts
+++ src/command/CommandList.ts
@@ -1,15 +1,9 @@
-// command interface
 import { Command } from "./Command";
-
-// Imported commands
 import { addCourse } from "../pieces/courseManagement/Addcourse";
 import { DiscussionMenu } from "../pieces/discussion/menu/DiscussionMenu";
 import { removeCourse } from "../pieces/courseManagement/RemoveCourse";
 import { testScore } from "../pieces/discussion/tracking/testScoreCommand";
 
-/**
- * @constant List of all the available commands
-*/
 export const commandList: Command[] = [ addCourse, removeCourse, DiscussionMenu, testScore];
 
 //some code taken from https://sabe.io/tutorials/how-to-build-discord-bot-typescript
