26c658ff89dc3c8af1b4be95ce43c98463830833
michael <ydna.mjs@gmail.com>
2023-06-30 15:16:51 -0400

    Replaced literals with constants in manage post scoring menu

--- src/pieces/discussion/menu/staff/managePostScoring/ManagePostScoringMenu.ts
+++ src/pieces/discussion/menu/staff/managePostScoring/ManagePostScoringMenu.ts
@@ -8,12 +8,28 @@ import { sendDismissableReply } from "../../../../../generalUtilities/Dismissabl
 const TITLE_COURSE_PREFIX = "Manage Post Scoring For CISC ";
 const MENU_DESCRIPTION = "replace me";
 
+const SCORE_PREFIX = "Post Points: ";
 const SCORE_DESCRIPTION = "The number of points awarded for a post that meets the requirements specified below";
+
+const COMMENT_POINTS_PREFIX = "Comment Points: ";
 const COMMENT_POINTS_DESCRIPTION = "The number of points awarded to the poster for comments others leave on their post";
+
+const LENGTH_REQ_PREFIX = "Length Requirement: ";
 const LENGTH_REQ_DESCRIPTION = "The minimum number of non white space characters required to recieve score points for a post";
+
+const PARAGRAPH_REQ_PREFIX = "Paragraph Requirement: ";
 const PARAGRAPH_REQ_DESCRIPTION = "The minimum number of paragraphs required to recieve points for a post";
+
+const LINK_REQ_PREFIX = "Link Requirement: ";
 const LINK_REQ_DESCRIPTION = "The minimum number of links required to recieve points for a post";
-const AWARD_DESCRIPTION = "Below this you will find a list of all awards and basic info about them";
+
+const AWARD_NAME = "Awards & Penalties"
+const AWARD_DESCRIPTION = "Below this you will find a list of all awards/penalities and basic info about them";
+
+const AWARD_POINT_PREFIX = "points: ";
+const AWARD_GIVERS_PREFIX = "\ngivers: "
+const AWARD_STAFFONLY_TEXT = "Staff Only"
+const AWARD_NOT_STAFFONLY_TEXT = "Everyone"
 
 export async function updateToManagePostScoringMenu(courseName: string, componentInteraction: MessageComponentInteraction) {
     
@@ -57,27 +73,27 @@ function generateFields(postSpecs: PostSpecs): APIEmbedField[] {
     
     const fields = [
         {
-            name: "Post Points: " + postSpecs.points,
+            name: SCORE_PREFIX + postSpecs.points,
             value: SCORE_DESCRIPTION
         },
         {
-            name: "Comment Points: " + postSpecs.commentPoints,
+            name: COMMENT_POINTS_PREFIX + postSpecs.commentPoints,
             value: COMMENT_POINTS_DESCRIPTION
         },
         {
-            name: "Length Requirement: " + postSpecs.minLength,
+            name: LENGTH_REQ_PREFIX + postSpecs.minLength,
             value: LENGTH_REQ_DESCRIPTION
         },
         {
-            name: "Paragraph Requirement: " + postSpecs.minParagraphs,
+            name: PARAGRAPH_REQ_PREFIX + postSpecs.minParagraphs,
             value: PARAGRAPH_REQ_DESCRIPTION
         },
         {
-            name: "Link Requirement " + postSpecs.minLinks,
+            name: LINK_REQ_PREFIX + postSpecs.minLinks,
             value: LINK_REQ_DESCRIPTION
         },
         {
-            name: "Awards & Penalties",
+            name: AWARD_NAME,
             value: AWARD_DESCRIPTION
         }
     ];
@@ -93,7 +109,7 @@ function makeAwardFields(postSpecs: PostSpecs) {
     const awardFields: APIEmbedField[] = awards.map((award)=> {
         return {
             name: award[0],
-            value: "points: " + award[1].points + "\ngivers: " + (award[1].trackStudents ? "Everyone" : "Staff Only")
+            value: AWARD_POINT_PREFIX + award[1].points + AWARD_GIVERS_PREFIX + (award[1].trackStudents ? AWARD_NOT_STAFFONLY_TEXT : AWARD_STAFFONLY_TEXT)
         }
     })
 
