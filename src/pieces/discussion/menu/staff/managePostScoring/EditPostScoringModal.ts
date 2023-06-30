6c41aa1b2ae8b2f6168a14ce402fcd3e5f8ca3f1
michael <ydna.mjs@gmail.com>
2023-06-30 16:20:12 -0400

    Added remaining fields for post scoring input

--- src/pieces/discussion/menu/staff/managePostScoring/EditPostScoringModal.ts
+++ src/pieces/discussion/menu/staff/managePostScoring/EditPostScoringModal.ts
@@ -1,11 +1,11 @@
 import { ActionRowBuilder, ButtonInteraction, TextInputBuilder } from "discord.js";
-import { openPostScoringModal, scoreInputActionRow } from "./ScoringModalUtilities";
+import { commentScoreInputActionRow, lengthReqInputActionRow, linkReqInputActionRow, openPostScoringModal, paraReqInputActionRow, scoreInputActionRow } from "./ScoringModalUtilities";
 
 const MODAL_ID_PREFIX = "test";
 const MODAL_TITLE_PREFIX = "";
 
 export async function openEditPostModal(courseName: string, triggerInteraction: ButtonInteraction) {
-    const components: ActionRowBuilder<TextInputBuilder>[] = [scoreInputActionRow];
+    const components: ActionRowBuilder<TextInputBuilder>[] = [scoreInputActionRow, commentScoreInputActionRow, lengthReqInputActionRow, paraReqInputActionRow, linkReqInputActionRow];
 
     openPostScoringModal(MODAL_ID_PREFIX, MODAL_TITLE_PREFIX, courseName, triggerInteraction, components, handleModalInput)
 }
--- src/pieces/discussion/menu/staff/managePostScoring/ScoringModalUtilities.ts
+++ src/pieces/discussion/menu/staff/managePostScoring/ScoringModalUtilities.ts
@@ -3,19 +3,75 @@ import { ModalInputHandler, createDiscussionModal } from "../../ModalUtilities";
 import { refreshManagePostScoringMenu, updateToManagePostScoringMenu } from "./ManagePostScoringMenu";
 import { DEFAULT_POST_SPECS } from "../../../../../pieces/courseManagement/DiscussionRulesDefaults";
 
-export const SCORE_INPUT_ID = "discussion_score_period_input";
-const SCORE_INPUT_LABEL = "score period #";
-const SCORE_INPUT_PLACEHOLDER = "0";
+// score input component
+export const SCORE_INPUT_ID = "discussion_score_input";
+const SCORE_INPUT_LABEL = "points for post";
 const SCORE_INPUT_STYLE = TextInputStyle.Short;
 
 const scoreInput = new TextInputBuilder({
-    customId: SCORE_INPUT_LABEL,
-    label: SCORE_INPUT_PLACEHOLDER,
+    customId: SCORE_INPUT_ID,
+    label: SCORE_INPUT_LABEL,
     placeholder: DEFAULT_POST_SPECS.points.toString(),
     style: SCORE_INPUT_STYLE,
 })
 
 export const scoreInputActionRow = new ActionRowBuilder<TextInputBuilder>({components: [scoreInput]});
+
+// comment score input component
+export const COMMENT_SCORE_INPUT_ID = "discussion_comment_score_input";
+const COMMENT_SCORE_INPUT_LABEL = "points for comments (given to poster)";
+const COMMENT_SCORE_INPUT_STYLE = TextInputStyle.Short;
+
+const commentScoreInput = new TextInputBuilder({
+    customId: COMMENT_SCORE_INPUT_ID,
+    label: COMMENT_SCORE_INPUT_LABEL,
+    placeholder: DEFAULT_POST_SPECS.commentPoints.toString(),
+    style: COMMENT_SCORE_INPUT_STYLE,
+})
+
+export const commentScoreInputActionRow = new ActionRowBuilder<TextInputBuilder>({components: [commentScoreInput]});
+
+// length req input component
+export const LENGTH_REQ_INPUT_ID = "discussion_length_input";
+const LENGTH_REQ_INPUT_LABEL = "post minimum length requirement";
+const LENGTH_REQ_INPUT_STYLE = TextInputStyle.Short;
+
+const lengthReqInput = new TextInputBuilder({
+    customId: LENGTH_REQ_INPUT_ID,
+    label: LENGTH_REQ_INPUT_LABEL,
+    placeholder: DEFAULT_POST_SPECS.minLength.toString(),
+    style: LENGTH_REQ_INPUT_STYLE,
+})
+
+export const lengthReqInputActionRow = new ActionRowBuilder<TextInputBuilder>({components: [lengthReqInput]});
+
+// paragraph req input component
+export const PARA_REQ_INPUT_ID = "discussion_paragraph_input";
+const PARA_REQ_INPUT_LABEL = "post minimum paragraph requirement";
+const PARA_REQ_INPUT_STYLE = TextInputStyle.Short;
+
+const paraReqInput = new TextInputBuilder({
+    customId: PARA_REQ_INPUT_ID,
+    label: PARA_REQ_INPUT_LABEL,
+    placeholder: DEFAULT_POST_SPECS.minParagraphs.toString(),
+    style: PARA_REQ_INPUT_STYLE,
+})
+
+export const paraReqInputActionRow = new ActionRowBuilder<TextInputBuilder>({components: [paraReqInput]});
+
+// link req input component
+export const LINK_REQ_INPUT_ID = "discussion_link_input";
+const LINK_REQ_INPUT_LABEL = "post minimum link requirement";
+const LINK_REQ_INPUT_STYLE = TextInputStyle.Short;
+
+const linkReqInput = new TextInputBuilder({
+    customId: LINK_REQ_INPUT_ID,
+    label: LINK_REQ_INPUT_LABEL,
+    placeholder: DEFAULT_POST_SPECS.minLinks.toString(),
+    style: LINK_REQ_INPUT_STYLE,
+})
+
+export const linkReqInputActionRow = new ActionRowBuilder<TextInputBuilder>({components: [linkReqInput]});
 
 export async function openPostScoringModal(idPrefix: string, titlePrefix: string, courseName: string, triggerInteraction: ButtonInteraction, components: ActionRowBuilder<TextInputBuilder>[], modalInputHandler: ModalInputHandler) {
     
